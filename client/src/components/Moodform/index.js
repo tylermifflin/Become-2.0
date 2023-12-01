012155ebb823ff6a52c6cfb7a0fea1c2d95040d8
Tyler Mifflin <you@example.com>
2023-11-30 19:37:12 -0700

    setting up state for moodtext and thought

--- client/src/components/Moodform/index.js
+++ client/src/components/Moodform/index.js
@@ -4,3 +4,13 @@ import { ADD_MOOD } from '../../utils/mutations';
 import { QUERY_MOODS, QUERY_ME } from '../../utils/queries';
 import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom';
+
+const MoodForm = () => {
+    // i want the mood to be a dropdown menu with 5 options
+    // i want the thought to be a text box
+
+    const [moodText, setMoodText] = useState('');
+    const [thought, setThought] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+    const moods = useQuery(QUERY_MOODS);
+    const me = useQuery(QUERY_ME);
