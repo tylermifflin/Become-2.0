9fae00878d87c7f13dd11d1059c52cb34b81cb10
tylermifflin <tyler.mifflin1@hotmail.com>
2023-12-01 19:37:30 -0700

    set up the mood page and imported the moodform, also set up a delete button for previous mood history

---
+++ client/src/pages/Moodpage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import MoodForm from '../components/Moodform';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_MOODS, QUERY_ME } from '../utils/queries';
+import { REMOVE_MOOD } from '../utils/mutations';
+
+const MoodPage = () => {
+    const { loading, data } = useQuery(QUERY_MOODS);
+    const [deleteMood] = useMutation(REMOVE_MOOD);
+    
+    const handleDelete = async (moodId) => {
+        try {
+        await deleteMood({
+            variables: { moodId },
+            refetchQueries: [{ query: QUERY_MOODS }, { query: QUERY_ME }],
+        });
+        } catch (e) {
+        console.error(e);
+        }
+    };
+    
+    return (
+        <div className='notHome'>
+        <MoodForm />
+        <div>
+            <h3>Current Moods:</h3>
+            {loading ? (
+            <p>Loading...</p>
+            ) : (
+            <ul>
+                {data.moods.map((mood) => (
+                <li key={mood._id}>
+                    <p>Mood: {mood.mood}</p>
+                    <p>Created On: {mood.createdAt}</p>
+                    <button onClick={() => handleDelete(mood._id)}>Delete</button>
+                </li>
+                ))}
+            </ul>
+            )}
+        </div>
+        </div>
+    );
+    };
+
+export default MoodPage;
