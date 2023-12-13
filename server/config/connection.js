f0cf7546b5c3775217135e1efe46ab8e742a8bd1
tylermifflin <tyler.mifflin1@hotmail.com>
2023-12-12 17:52:29 -0700

    test to see if the public folder was messing up the proxy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// MongoDB Atlas connection string
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/become');
 
 module.exports = mongoose.connection;
