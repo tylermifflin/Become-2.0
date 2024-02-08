668f6257924aed90e405478a245444f4ae9a1adf
tylermifflin <tyler.mifflin1@hotmail.com>
2024-02-07 17:55:22 -0700

    fixed the bug on my moodform, the query me was messing it up

--- client/src/components/Moodform/index.js
+++ client/src/components/Moodform/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import { ADD_MOOD } from '../../utils/mutations';
-import { QUERY_MOODS, QUERY_ME } from '../../utils/queries';
+import { QUERY_MOODS} from '../../utils/queries';
 import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom';
 
@@ -10,7 +10,6 @@ const MoodForm = () => {
     const [thought, setThought] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
     const moods = useQuery(QUERY_MOODS);
-    const me = useQuery(QUERY_ME);
 
     const [addMood, { error }] = useMutation(ADD_MOOD, {
         update(cache, { data: { addMood } }) {
@@ -23,12 +22,6 @@ const MoodForm = () => {
             } catch (e) {
                 console.error(e);
             }
-
-            console.log(me);
-            cache.writeQuery({
-                query: QUERY_ME,
-                data: { me: { ...me, moods: [...me.data.me.moods, addMood] } }
-            });
         }
     });
 
