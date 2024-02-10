08838a24678ff99492e9a461b14dc47dd3336c39
tylermifflin <tyler.mifflin1@hotmail.com>
2024-02-10 16:25:22 -0700

    got rid of excess css i wasn't using on my button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 // importing react
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo2 from ".././images/logo2.png";
+import logo from ".././images/logo.png";
 
 
 
@@ -10,14 +10,14 @@ function Home() {
   // render the home page
   return (
       <div className="flex-row justify-center">
-        <div>
+        <div className="container-fluid">
           <h1 className="text-center card-title">A User Friendly Self Helf App</h1>
           <p className="text-center">
             Welcome to Become, our goal is to help you become the best version of yourself. 
             You will increase your awareness, motivation, and feel more inspired to conquer your challenges. 
             Join us and unlock your unlimited potential. 
           </p>
-          <img src={logo2} alt="logo" width="100" ></img>
+          <img src={logo} alt="logo" width="100" ></img>
         </div>
       </div>
   );
